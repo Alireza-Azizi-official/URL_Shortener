52719050d7324c6d7635768ab4e0dd1f99d649df
alireza.azizi <alireza.azizi@pnr.ir>
2025-12-03 20:19:53 +0330

    redis added.

--- url_shortener/app/main.py
+++ url_shortener/app/main.py
@@ -3,12 +3,14 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app.db import create_tables_if_not_exist
+from app.dependencies import init_redis
 from app.middleware import VisitLoggingMiddleware
-from app.shortener import router
+from url_shortener.app.router import router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    app.state.redis = await init_redis()
     await create_tables_if_not_exist()
     yield
 
