8731a6f64da104fbaf232efe0fdc914f7c24639b
alireza.azizi <alireza.azizi@pnr.ir>
2025-12-03 20:20:28 +0330

    redis got dependencies

---
+++ url_shortener/app/dependencies.py
@@ -0,0 +1,19 @@
+from fastapi import Request
+from redis.asyncio import from_url
+
+from app.config import settings
+
+
+async def init_redis():
+    global _redis_client
+    if _redis_client is None:
+        _redis_client = await from_url(
+            settings.REDIS_URL,
+            encoding="utf-8",
+            decode_responses=True,
+        )
+    return _redis_client
+
+
+async def get_redis(request: Request):
+    return request.app.state.redis
