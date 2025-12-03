f1b423f62d4164f2a7091425c6e12c4f8c1c2f2b
alireza.azizi <alireza.azizi@pnr.ir>
2025-12-03 18:10:18 +0330

    db engine, session added

--- url_shortener/app/db.py
+++ url_shortener/app/db.py
@@ -1,19 +1,35 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker
+import asyncio
 
-from app.config import settings
+import sqlalchemy as sa
+from sqlalchemy.ext.asyncio import AsyncEngine, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import declarative_base
 
-engine = create_async_engine(
-    settings.DATABASE_URL,
+from .config import settings
+
+Base = declarative_base()
+
+
+DATABASE_URL = settings.DATABASE_URL
+
+engine: AsyncEngine = create_async_engine(
+    DATABASE_URL,
     echo=False,
-    pool_size=20,
-    max_overflow=10,
-    pool_pre_ping=True,
+    pool_size=int(settings.DB_POOL_SIZE),
+    max_overflow=int(settings.DB_MAX_OVERFLOW),
+    pool_timeout=int(settings.DB_POOL_TIEMOUT),
+    future=True,
 )
 
-AsyncSessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine, expire_on_commit=False, class_=sa.ext.asyncio.AsyncSession
+)
 
 
-async def get_db():
+async def create_tables_if_not_exist():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def get_session():
     async with AsyncSessionLocal() as session:
         yield session
