0988954690483ee08096e21a46ea181bddcc589c
alireza.azizi <alireza.azizi@pnr.ir>
2025-12-03 23:00:42 +0330

    prefix got removed

--- url_shortener/app/router.py
+++ url_shortener/app/router.py
@@ -10,7 +10,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import RedirectResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
-router = APIRouter(prefix='/api/v1', tags=['URL'])
+router = APIRouter(tags=['URL'])
 
 
 @router.post("/shorten", response_model=ShortenResponse)
