8e15c64bd27c9123235739a0908eca6dc092980f
alireza.azizi <alireza.azizi@pnr.ir>
2025-12-03 19:52:58 +0330

    kafka producer added.

--- url_shortener/app/kafka_producer.py
+++ url_shortener/app/kafka_producer.py
@@ -1,11 +1,44 @@
-import asyncio
+import json
+from typing import Optional
 
-from app.config import settings
+from aiokafka import AIOKafkaProducer
+from fastapi import FastAPI
+
+from .config import settings
 
 KAFKA_ENABLED = settings.KAFKA_ENABLED
+KAFKA_BOOTSTRAP = settings.KAFKA_BOOTSTRAP_SERVERS
+KAFKA_TOPIC = settings.KAFKA_VISIT_TOPIC
 
 
-async def publish_visit_events(event: dict):
+producer = Optional[AIOKafkaProducer] = None
+
+
+async def init_kafka(app: FastAPI):
+    global producer
     if not KAFKA_ENABLED:
         return
-    await asyncio.sleep(0)
+
+    producer = AIOKafkaProducer(
+        bootstrap_server=KAFKA_BOOTSTRAP,
+        value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+        linger_msg=5,
+        acks="all",
+    )
+    await producer.start()
+    app.state.kafka_producer = producer
+
+
+async def close_kafka(app: FastAPI):
+    if producer:
+        await producer.stop()
+
+
+async def publish_visit_event(event: dict):
+    if not KAFKA_ENABLED or not producer:
+        return
+
+    try:
+        await producer.send_and_wait(KAFKA_TOPIC, event)
+    except Exception as e:
+        print(f"[KAFKA EROR] {str(e)}")
