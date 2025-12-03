fb71a30b06bf83c7820b3932c51122b31689d6db
alireza.azizi <alireza.azizi@pnr.ir>
2025-12-03 18:29:42 +0330

    url, visit_urls table added

--- url_shortener/app/models.py
+++ url_shortener/app/models.py
@@ -1,29 +1,28 @@
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
-from sqlalchemy.orm import declarative_base, relationship
-from sqlalchemy.sql import func
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, func
+from sqlalchemy.orm import relationship
 
-Base = declarative_base()
+from .db import Base
 
 
 class URL(Base):
     __tablename__ = "urls"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    original_url = Column(String, nullable=False)
-    short_code = Column(String, nullable=False, unique=True, index=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    original_url = Column(String(2048), nullable=False)
+    short_code = Column(String(20), nullable=False, unique=True, index=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+    visits_count = Column(Integer, nullable=False, server_default=0)
 
-    visits = relationship(
-        "URLVisit", back_populates="urls", cascade="all, delete-orphan"
-    )
+    visit_logs = relationship("VisitLog", back_populates="urls", cascade="all, delete")
 
 
-class URLVisit(Base):
-    __tablename__ = "url_visits"
+class VisitiLog(Base):
+    __tablename__ = "visit_logs"
 
-    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    url_id = Column(Integer, ForeignKey("urls.id"), nullable=False)
-    ip_address = Column(String, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    url_id = Column(Integer, ForeignKey("urls.id", ondelete="CASCADE"), nullable=False, index=True)
     timestamp = Column(DateTime(timezone=True), server_default=func.now())
+    ip = Column(String(45), nullable=True)
+    user_agent = Column(String(512), nullable=True)
 
-    url = relationship("URL", back_populates="visits")
+    url = relationship('URL', back_populates='visit_logs')
