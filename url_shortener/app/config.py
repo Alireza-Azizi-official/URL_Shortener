e80c0aa6e715be3956d81993caadeb9267646ccb
alireza.azizi <alireza.azizi@pnr.ir>
2025-12-03 21:20:00 +0330

    config file got completed and fixed

--- url_shortener/app/config.py
+++ url_shortener/app/config.py
@@ -1,17 +1,17 @@
-from pydantic import Field, PostgresDsn
 from pydantic_settings import BaseSettings
 
-
 class Settings(BaseSettings):
-    DATABASE_URL: PostgresDsn
-    REDIS_URL: str = Field("redis://localhost:6379/0")
-    DB_POOL_SIZE: int = 10
-    DB_MAX_OVERFLOW: int = 20
-    DB_POOL_TIEMOUT: int = 30
+    DATABASE_URL: str
+    REDIS_URL: str = "redis://localhost:6379/0"
+    DB_POOL_SIZE: int = 20
+    DB_MAX_OVERFLOW: int = 40
+    DB_POOL_TIMEOUT: int = 30
     APP_HOST: str = "0.0.0.0"
-    APP_HOST: int = 8000
+    APP_PORT: int = 8000
     BASE_URL: str = "http://localhost:8000"
     KAFKA_ENABLED: bool = False
+    KAFKA_BOOTSTRAP_SERVER: str = 'localhost:9092'
+    KAFKA_VISIT_TOPIC: str = 'visit_events'
 
     class Config:
         env_file = ".env"
