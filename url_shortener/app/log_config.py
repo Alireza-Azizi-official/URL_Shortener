127f9f83d43170369bbc3a0b41c88b670097445e
alireza.azizi <alireza.azizi@pnr.ir>
2025-12-04 13:39:37 +0330

    log config added to the app

---
+++ url_shortener/app/log_config.py
@@ -0,0 +1,11 @@
+import logging
+
+logger = logging.getLogger("url_shortener")
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("[%(asctime)s] [%(levelname)s][%(name)s] %(message)s")
+console_handler.setFormatter(formatter)
+if not logger.handlers:
+    logger.addHandler(console_handler)
