f44d31b05f1fdc9f5df390c3ffd997be0779e83e
alireza.azizi <alireza.azizi@pnr.ir>
2025-12-06 14:53:53 +0330

    limiter got independent from main app

--- url_shortener/app/slowapi_conf.py
+++ url_shortener/app/slowapi_conf.py
@@ -1,12 +1,6 @@
 import os
 
-from slowapi import Limiter, _rate_limit_exceeded_handler
-from slowapi.errors import RateLimitExceeded
-from slowapi.util import get_remote_address
-from app.main import app
+from slowapi import Limiter
 
 REDIS_URL = os.getenv("REDIS_URL")
-limiter = Limiter(key_func=get_remote_address, storage_uri=REDIS_URL)
-
-app.state.limiter = limiter
-app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+limiter = Limiter(key_func=lambda request: request.client.host, storage_uri=REDIS_URL)
